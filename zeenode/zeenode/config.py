a67065c7a625eccc4293c3fb3238083c118cd063
scam69 <86785919+scam69@users.noreply.github.com>
2021-07-01 21:46:17 +0530

    Update config.py

--- zeenode/zeenode/config.py
+++ zeenode/zeenode/config.py
@@ -1,5 +1,5 @@
 class config:
-    global auth; auth = "" # Enter your discord token for Auto-Login.
-    global prefix; prefix = "$" # Enter your prefix for selfbot.
+    global auth; auth = "NzczMDQ3NTQwMDY5MzAyMjky.YN3nYA.EgP3iyw8jJXJ6d9juEJD-JD_KJU" # Enter your discord token for Auto-Login.
+    global prefix; prefix = "." # Enter your prefix for selfbot.
     global nitro_sniper; nitro_sniper = "true" # 'true' to enable nitro sniper, 'false' to disable.
     global giveaway_sniper; giveaway_sniper = "true" # 'true' to enable giveaway sniper, 'false' to disable.
